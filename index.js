867569eec18d7aff377d93e8f23375dcb15c74aa
skiddedruck <overrock101@gmail.com>
2024-08-13 01:44:36 -0400

    WL

--- index.js
+++ index.js
@@ -3,59 +3,85 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
 const port = 3000;
 
-let { whitelistedUsers } = require('./whitelist.json');
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-app.get('/whitelist/:userId', (req, res) => {
-  res.json({ whitelisted: whitelistedUsers.includes(req.params.userId) });
-});
+// Load whitelist.json
+let whitelistData = require('./whitelist.json');
 
-app.post('/whitelist/:userId', async (req, res) => {
-  if (!req.headers.authorization) {
-    res.sendStatus(400);
-    return;
-  }
-  if (req.headers.authorization === `WlKey ${process.env.WL_KEY}`) {
-    whitelistedUsers.push(req.params.userId);
-    await saveUsers();
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(401);
-  }
-});
-
-app.post('/unwhitelist/:userId', async (req, res) => {
-  if (!req.headers.authorization) {
-    res.sendStatus(400);
-    return;
-  }
-  if (req.headers.authorization === `WlKey ${process.env.WL_KEY}`) {
-    whitelistedUsers = whitelistedUsers.filter(
-      (userId) => userId !== req.params.userId
-    );
-    await saveUsers();
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(401);
-  }
-});
-
-const saveUsers = async () => {
+// Helper function to save whitelist data
+const saveWhitelist = async () => {
   try {
-    await fs.writeFile(
-      'whitelist.json',
-      JSON.stringify({ whitelistedUsers: whitelistedUsers }, null, 2)
-    );
+    await fs.writeFile('whitelist.json', JSON.stringify(whitelistData, null, 2));
   } catch (err) {
-    console.error('Error saving users:', err);
+    console.error('Error saving whitelist:', err);
   }
 };
 
-process.on('SIGTERM', saveUsers);
-process.on('SIGINT', saveUsers);
+// Endpoint to check if a user is whitelisted
+app.get('/whitelist/:discordUserId', (req, res) => {
+  const user = whitelistData.WhitelistedUsers[req.params.discordUserId];
+  if (user) {
+    res.json({ whitelisted: true, user });
+  } else {
+    res.json({ whitelisted: false });
+  }
+});
+
+// Endpoint to add a user to the whitelist
+app.post('/whitelist/:discordUserId', async (req, res) => {
+  if (!req.headers.authorization) {
+    res.sendStatus(400);
+    return;
+  }
+  if (req.headers.authorization === `WlKey ${process.env.WL_KEY}`) {
+    const { hash, attackable, level, tags } = req.body;
+    
+    if (!hash || typeof attackable !== 'boolean' || typeof level !== 'number' || !Array.isArray(tags)) {
+      res.sendStatus(400); // Bad request
+      return;
+    }
+    
+    whitelistData.WhitelistedUsers[req.params.discordUserId] = {
+      hash,
+      attackable,
+      level,
+      tags
+    };
+    
+    await saveWhitelist();
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+// Endpoint to remove a user from the whitelist
+app.post('/unwhitelist/:discordUserId', async (req, res) => {
+  if (!req.headers.authorization) {
+    res.sendStatus(400);
+    return;
+  }
+  if (req.headers.authorization === `WlKey ${process.env.WL_KEY}`) {
+    if (whitelistData.WhitelistedUsers[req.params.discordUserId]) {
+      delete whitelistData.WhitelistedUsers[req.params.discordUserId];
+      await saveWhitelist();
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404); // Not found
+    }
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+// Endpoint to fetch all tags
+app.get('/tags', (req, res) => {
+  res.json(whitelistData.WhitelistedTags);
+});
 
 app.listen(port, () => {
-  console.log('WL listening');
+  console.log(`WL listening on port ${port}`);
 });
